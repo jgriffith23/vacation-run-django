c47ec08d00315849eb80f885bc8266e1f0c3e4b0
Jennifer <jennifer.gd42@gmail.com>
2017-12-14 14:48:28 -0800

    Convert functional tests to use unittest framework

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,18 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewUserTest(unittest.TestCase):
 
-assert 'Django' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_visit_homepage(self):
+        self.browser.get("http://localhost:8000")
+        self.assertIn("Vacation Run", self.browser.title)
+        self.fail("Finish writing this test!")
+
+if __name__ == "__main__":
+    unittest.main(warnings="ignore")
