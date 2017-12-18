7c2ca427ca8d40a85eaeebd8011b58044a91d14b
Jennifer <jennifer.gd42@gmail.com>
2017-12-17 20:55:35 -0800

    Fix root resolution test; finish template test for homepage

--- runtracker/tests.py
+++ runtracker/tests.py
@@ -1,8 +1,17 @@
 from django.test import TestCase
 from django.urls import resolve
 from runtracker.views import HomePageView
+from django.test import Client
 
 class HomePageTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+
     def test_root_resolves_to_home(self):
-         found = resolve('/')
-         self.assertEqual(found.view_name, "home")
+         result = resolve("/")
+         self.assertEqual(result.func.view_class, HomePageView)
+
+    def test_homepage_has_correct_template(self):
+        result = self.client.get("/")
+        self.assertIn(b"<title>Vacation Run</title", result.content)
+
