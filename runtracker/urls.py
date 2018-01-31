772e696bcd151302c41f5da36ed4d8cb777260ac
Jennifer <jennifer.gd42@gmail.com>
2018-01-31 10:51:12 -0800

    Workoutlist view is accessible but not able to display workouts.

--- runtracker/urls.py
+++ runtracker/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.HomePageView.as_view(), name="home")
+    url(r'^$', views.HomePageView.as_view(), name="home"),
+    url(r'^workouts', views.WorkoutListView.as_view(), name="workout_list"),
 ]
--- runtracker/views.py
+++ runtracker/views.py
@@ -1,6 +1,6 @@
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
-
+from runtracker.models import Workout
 
 class HomePageView(generic.TemplateView):
     template_name = "runtracker/home.html"
@@ -8,5 +8,9 @@ class HomePageView(generic.TemplateView):
 class WorkoutListView(LoginRequiredMixin, generic.ListView):
     """An exerciser's logged workouts. Only viewable by authenticated users."""
 
+    template_name = "runtracker/workout_list.html"
 
+    def get_queryset(self):
+        print(self.request.user.exerciser)
+        return Workout.objects.all().filter(exerciser_id=self.request.user.exerciser)
 
