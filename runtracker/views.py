d2bf5596dfb5b34185d59c19e51079ae84ef955d
Jennifer <jennifer.gd42@gmail.com>
2018-02-08 14:04:32 -0800

    Show upcoming events on dashboard

--- runtracker/views.py
+++ runtracker/views.py
@@ -1,6 +1,7 @@
 from django.views import generic
+from datetime import datetime
 from django.contrib.auth.mixins import LoginRequiredMixin
-from runtracker.models import Workout
+from runtracker.models import Workout, Event
 from runtracker.forms import WorkoutForm
 
 
@@ -14,11 +15,20 @@ class DashboardView(LoginRequiredMixin, generic.ListView):
 
     template_name = "runtracker/dashboard.html"
 
-    context_object_name = "workouts"
+    context_object_name = "actions"
 
     def get_queryset(self):
         print(self.request.user.exerciser)
-        return Workout.objects.all().filter(exerciser=self.request.user.exerciser)
+
+        workouts = Workout.objects.all().filter(
+            exerciser=self.request.user.exerciser
+        )
+
+        events = Event.objects.all().filter(
+            exercisers=self.request.user.exerciser,
+            date__gte=datetime.now(),
+        )
+        return {"workouts": workouts, "events": events}
 
     def get_context_data(self, **kwargs):
         context = super(DashboardView, self).get_context_data(**kwargs)
